fefd1662d7f58ae498e4a776e807f070047e9f0c
Ramón <ramonpintado91@gmail.com>
2019-03-17 16:55:36 +0100

    NETIFLYKEY test

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,12 @@ echo "You are on branch $TRAVIS_BRANCH"
 echo "The TRAVIS_PULL_REQUEST_BRANCH is $TRAVIS_PULL_REQUEST_BRANCH"
 if [[ "$TRAVIS_PULL_REQUEST_BRANCH" = "" ]]
 then
-	  echo "You are on master, deploying production."
-	  netlify deploy --prod --dir=docs
+	zip -r r-ladies.zip docs/
+
+curl -H "Content-Type: application/zip" \
+	 -H "Authorization: Bearer $NETLIFYKEY" \
+	 --data-binary "@r-ladies.zip" \
+     https://api.netlify.com/api/v1/sites/<Your site domain name>/deploys
 else
     echo "You are not on master, deploying preview."
-    netlify deploy --dir=docs
 fi
